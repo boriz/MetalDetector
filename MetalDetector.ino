a91cd82fe5cd31bab04eb6a1d736af4780234cf7
boriz <cherkasskiy@gmail.com>
2018-10-02 23:19:52 -0600

    Initial commit. Empty repo.

---
+++ MetalDetector.ino
@@ -0,0 +1,23 @@
+#include "TimerOne.h"
+
+void setup() 
+{
+  // put your setup code here, to run once:  
+  pinMode(9, OUTPUT);
+  Timer1.initialize(4); // 4us = 250KHz
+  Timer1.pwm(9, 512);
+  Serial.begin(115200);
+}
+
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+      
+  Serial.println(analogRead(A0));   
+  delay(500); 
+
+  //tone(9,100000);
+  // delayMicroseconds(200);
+ 
+}
